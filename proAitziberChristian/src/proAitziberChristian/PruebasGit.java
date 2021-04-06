39f1762a5adc3f0a940c3f139ca047cef4e580bf
cjodra14 <64621611+cjodra14@users.noreply.github.com>
2021-04-06 17:05:38 +0200

    Primer Cambio

---
+++ proAitziberChristian/src/proAitziberChristian/PruebasGit.java
@@ -0,0 +1,10 @@
+package proAitziberChristian;
+
+import java.awt.EventQueue;
+
+public class PruebasGit {
+
+	public static void main(String[] args) {
+		System.out.println("Primer Mensaje");
+	}
+}
