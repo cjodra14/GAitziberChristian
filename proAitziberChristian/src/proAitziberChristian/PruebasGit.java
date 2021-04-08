8ea324bca0859c70b661bc7a58aa5f46f1ca6775
cjodra14 <64621611+cjodra14@users.noreply.github.com>
2021-04-08 23:58:06 +0200

    Quinto cambio Solucion

--- proAitziberChristian/src/proAitziberChristian/PruebasGit.java
+++ proAitziberChristian/src/proAitziberChristian/PruebasGit.java
@@ -5,12 +5,7 @@ import java.awt.EventQueue;
 public class PruebasGit {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Quinto Mensaje Conflicto");
 		System.out.println("Quinto Mensaje");
-		
-=======
-		System.out.println("Quinto Mensaje");
->>>>>>> refs/remotes/origin/master
 	}
 }
