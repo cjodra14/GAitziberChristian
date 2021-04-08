7a2fba46e65cc1251bb656e65e6bdd0a9423fecd
aitzi <aitzi@DESKTOP-LHHQ5ER>
2021-04-09 00:16:58 +0200

    sexto cambio

--- proAitziberChristian/src/proAitziberChristian/PruebasGit.java
+++ proAitziberChristian/src/proAitziberChristian/PruebasGit.java
@@ -5,7 +5,6 @@ import java.awt.EventQueue;
 public class PruebasGit {
 
 	public static void main(String[] args) {
-		System.out.println("Quinto Mensaje Conflicto");
-		System.out.println("Quinto Mensaje");
+		System.out.println("Sexto Mensaje");
 	}
 }
